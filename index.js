a365beb6d11fc6716fb477e4cb2b9fcad1614375
ehabwasel <ehab.wasel@yahoo.com>
2021-10-01 00:27:24 +0200

    first project with vue

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+    data(){
+        return {
+          firstName: 'ehab',
+          lastName: 'wasel',
+          email: 'ehab.wasel@yahoo.com',
+          gender: 'mail',
+          picture: 'https://randomuser.me/api/portraits/men/43.jpg',
+          phone: '093104',
+          age: '32',
+        };
+    },
+    methods:{
+ async getUser(){
+    const res=await fetch('https://randomuser.me/api')
+    const {results} = await res.json()
+    console.log(results)
+    this.firstName= results[0].name.first
+    this.lastName= results[0].name.last
+    this.email= results[0].email
+    this.gender= results[0].gender
+    this.age = results[0].dob.age;
+    this.phone= results[0].phone
+    this.picture= results[0].picture.large
+}
+
+    }
+})
+app.mount('#app')
